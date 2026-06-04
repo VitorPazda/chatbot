adcd96f13acfa52ec6cc14947a7f45677eb5f015
Nogrilo <nogriloesirjeferson@gmail.com>
2026-06-04 16:23:59 -0300

    fazendo o prompt base, carregando o bd na main

--- main.py
+++ main.py
@@ -3,3 +3,30 @@
 # pip install python-dotenv langchain langchain-openai langchain-community langchain-chroma chromadb openai pypdf
 
 # chroma é o responsável em vetorizar o banco de dados
+
+from langchain_chroma.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+databasePath = "db"
+
+promptTemplate = """
+Responda a pergunta do usuário
+{pergunta}
+
+com base nessas informações
+
+{baseConhecimento}
+
+Caso não encontre a resposta para a pergunta do usuário nessas informações, 
+responda que não sabe dizer."""
+
+question = input("Escreva sua pergunta: ")
+
+# carregar db
+embeddingFunction = OpenAIEmbeddings()
+db = Chroma(persiste_directory=databasePath, embedding_function=embeddingFunction)
+
+# comparar a pergunta do usuario, com o db
