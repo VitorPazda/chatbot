3e25af0efa8fff3b91f2aa46bee4f921775a4602
Nogrilo <nogriloesirjeferson@gmail.com>
2026-06-04 16:04:27 -0300

    add a funcao de dividir toda a minha base de dados em pdf, em chunks

--- createDb.py
+++ createDb.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 baseFolder = "base"
 
@@ -7,13 +8,25 @@ def loadDocuments():
     documents = loader.load()
     return documents
 
+def splitChunks(documents):
+    documentSplitter = RecursiveCharacterTextSplitter(
+        chunk_size = 2000, 
+        chunk_overlap = 500,
+        length_function = len,
+        add_start_index = True
+    )
+
+    chunks = documentSplitter.split_documents(documents)
+    print(len(chunks))
+    return chunks
+
+def vectorizeChunks(chunks):
+    pass
+
 def createDb():
     documents = loadDocuments()
     print(documents)
-    #chunks = splitChunks(documents)
-    #vectorizeChunks(chunks)
-    # carregar os documentos
-    # dividir os documentos em pedacos
-    pass
+    chunks = splitChunks(documents)
+    vectorizeChunks(chunks)
 
 createDb()
