c00f52348ed6231e9f99aa8b5df988cfc873a67a
Nogrilo <nogriloesirjeferson@gmail.com>
2026-06-04 15:43:36 -0300

    fazendo a funcao de carregar documentos, e add um pdf de como jogar de gragas, só para teste

--- createDb.py
+++ createDb.py
@@ -4,11 +4,14 @@ baseFolder = "base"
 
 def loadDocuments():
     loader = PyPDFDirectoryLoader(baseFolder, glob="*.pdf")
+    documents = loader.load()
+    return documents
 
 def createDb():
     documents = loadDocuments()
-    chunks = splitChunks(documents)
-    vectorizeChunks(chunks)
+    print(documents)
+    #chunks = splitChunks(documents)
+    #vectorizeChunks(chunks)
     # carregar os documentos
     # dividir os documentos em pedacos
     pass
