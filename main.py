5b6f2a8a804d2f34c585ce1d4e7164019da8fd21
Nogrilo <nogriloesirjeferson@gmail.com>
2026-06-04 15:39:08 -0300

    att o python e instalando todas as bilbliotecas necessarias

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # todas as biliotecas necessárias
 
-# pip install python-dotenv langchain langchain-openai langchain-community langchain-chroma chromadb open ai pypdf
+# pip install python-dotenv langchain langchain-openai langchain-community langchain-chroma chromadb openai pypdf
 
 # chroma é o responsável em vetorizar o banco de dados
