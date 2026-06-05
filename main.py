5f0368da78e6491235b4595d804e7b0e9df7883b
Nogrilo <nogriloesirjeferson@gmail.com>
2026-06-05 09:43:28 -0300

    modifando o codigo para hugging face, mas ainda sem grandes progressos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # todas as biliotecas necessárias
 
-# pip install python-dotenv langchain langchain-openai langchain-community langchain-chroma chromadb openai pypdf
+# pip install python-dotenv langchain langchain-openai langchain-community langchain-chroma chromadb openai pypdf langchain-ollama
 
 # Teste hugging face
 # pip install langchain-huggingface sentence-transformers
@@ -9,7 +9,10 @@
 
 from langchain_chroma.vectorstores import Chroma
 # from langchain_openai import OpenAIEmbeddings
+
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,7 +21,7 @@ databasePath = "db"
 
 promptTemplate = """
 Responda a pergunta do usuário
-{pergunta}
+{question}
 
 com base nessas informações
 
@@ -27,14 +30,31 @@ com base nessas informações
 Caso não encontre a resposta para a pergunta do usuário nessas informações, 
 responda que não sabe dizer."""
 
-question = input("Escreva sua pergunta: ")
+def ask():
+    question = input("Escreva sua pergunta: ")
 
-# carregar db
-# embeddingFunction = OpenAIEmbeddings()
-embeddingFunction = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-db = Chroma(persist_directory=databasePath, embedding_function=embeddingFunction)
+    # carregar db
+    # embeddingFunction = OpenAIEmbeddings()
+    embeddingFunction = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    db = Chroma(persist_directory=databasePath, embedding_function=embeddingFunction)
 
-# comparar a pergunta do usuario, com o db
-results = db.similarity_search_with_relevance_scores(question, k=3)
-print(results)
-print(len(results))
+    # comparar a pergunta do usuario, com o db
+    results = db.similarity_search_with_relevance_scores(question, k=3)
+    #if len(results) == 0 or results[0][1] < 0.7:
+    #    print("Não achei nada relevante na base de dados")
+    #    return 
+    
+    textsResult = []
+    for result in results:
+        text = result[0].page_content
+        textsResult.append(text)
+
+    baseConhecimento = "\n\n---\n\n".join(textsResult)
+    prompt = ChatPromptTemplate.from_template(promptTemplate)
+    prompt = prompt.invoke({"question": question, "baseConhecimento": baseConhecimento})
+
+    model = ChatOllama()
+    textAnswer = model.invoke(prompt)
+    print(textAnswer)
+    
+ask()
